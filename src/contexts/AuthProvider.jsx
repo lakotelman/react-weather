9d6670a3f0e7b68f828a6d6f40b2040f7314d309
lakotelman <la.n.kot@pm.me>
2022-07-29 13:00:31 -0800

    wip firebase auth

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,1 +1,16 @@
-import {GoogleAuthProvider} from "firebase/auth"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "@firebase/auth";
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = (props) => {
+  const [user, setUser] = useState({ loggedIn: false });
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  const values = {};
+
+  return (
+    <AuthContext.Provider value={values}>{props.children}</AuthContext.Provider>
+  );
+};
